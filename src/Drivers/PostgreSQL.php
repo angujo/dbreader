5177679f9e0c67c7153d91fad92a3c0a584a9d35
bangujo <bangujo@kemri-wellcome.org>
2019-12-19 15:57:24 +0300

    clean PostgreSQL

--- src/Drivers/PostgreSQL.php
+++ src/Drivers/PostgreSQL.php
@@ -14,11 +14,11 @@ use Angujo\DBReader\Models\Schema;
 
 class PostgreSQL extends Dbms
 {
-    private        $_schema;
-    protected      $functions              = ['now', 'nextval', 'currval', 'setval',];
-    private static $tables_query           = 'SELECT t.schemaname schema_name, t.tablename "name", true is_table, false is_view FROM pg_catalog.pg_tables t';
-    private static $views_query            = 'SELECT t.schemaname schema_name, t.viewname "name", false is_table, true is_view FROM pg_catalog.pg_views t';
-    private static $columns_query          = 'select n.nspname schema_name,t.relname table_name, ty.typname data_type, c.attname "name", c.attlen "length", c.attnotnull = false is_nullable, c.attnum ordinal, cmt.description "comment", '.
+    private $_schema;
+    private static $tables_query = 'SELECT t.schemaname schema_name, t.tablename "name", true is_table, false is_view FROM pg_catalog.pg_tables t';
+    private static $views_query = 'SELECT t.schemaname schema_name, t.viewname "name", false is_table, true is_view FROM pg_catalog.pg_views t';
+    private static $columns_query = 'select n.nspname schema_name,t.relname table_name, ty.typname data_type, c.attname "name", c.attlen "length", '.
+    'c.attnotnull = false is_nullable, c.attnum ordinal, cmt.description "comment", '.
     'pg_get_expr(d.adbin, d.adrelid)::information_schema.character_data "default", st.oid is not null is_auto_increment '.
     'from pg_catalog.pg_attribute c '.
     'join pg_catalog.pg_class t on t.oid=c.attrelid join pg_catalog.pg_namespace n on n.oid=t.relnamespace '.
@@ -27,26 +27,27 @@ class PostgreSQL extends Dbms
     'left join pg_catalog.pg_type st on pg_get_serial_sequence(t.relname,c.attname)=n.nspname||\'.\'||st.typname '.
     'where not c.attisdropped and NOT pg_is_other_temp_schema(n.oid) AND c.attnum > 0 AND '.
     '(t.relkind = ANY (ARRAY[\'r\'::"char", \'v\'::"char", \'f\'::"char", \'p\'::"char"]))';
-    private static $constraints_query      = 'select n.nspname schema_name, t.relname table_name, a.attname column_name, c.conname "name", c.consrc check_source, '.
+    private static $constraints_query = 'select n.nspname schema_name, t.relname table_name, a.attname column_name, c.conname "name", c.consrc check_source, '.
     '\'f\'::character=c.contype is_foreign_key, \'u\'::character=c.contype is_unique_key, \'p\'::character=c.contype is_primary_key ,\'c\'::character=c.contype is_check'.
     'from pg_catalog.pg_constraint c join pg_catalog.pg_namespace n on n.oid=c.connamespace join pg_catalog.pg_class t on t.oid=conrelid '.
     'join pg_catalog.pg_attribute a on a.attrelid=t.oid and (a.attnum = any (c.conkey)) '.
     'where (c.contype = any (array[\'p\'::character, \'u\'::character, \'c\'::character]))';
-    private static $toone_foreign_queries  = 'select a.ordinal, n.nspname schema_name, t.relname table_name, a.attname column_name, c.conname "name", rn.nspname foreign_schema_name, rt.relname foreign_table_name, '.
+    private static $toone_foreign_queries = 'select a.ordinal, n.nspname schema_name, t.relname table_name, a.attname column_name, c.conname "name", rn.nspname foreign_schema_name, rt.relname foreign_table_name, '.
     'ra.attname foreign_column_name '.
     'from pg_catalog.pg_constraint c join pg_catalog.pg_namespace n on n.oid=c.connamespace join pg_catalog.pg_class t on t.oid=c.conrelid '.
     'join pg_catalog.pg_attribute a on a.attrelid=t.oid and a.attnum = any (c.conkey) join pg_catalog.pg_class rt on rt.oid=c.confrelid '.
     'join pg_catalog.pg_namespace rn on rn.oid=rt.relnamespace join pg_catalog.pg_attribute ra on ra.attrelid=rt.oid and ra.attnum = any (c.confkey) '.
     'left join pg_catalog.pg_constraint uc on n.oid=uc.connamespace and a.attnum = any (uc.conkey) and t.oid=uc.conrelid and not uc.contype = any (array[\'u\'::character,\'p\'::character]) '.
     'where (c.contype =\'f\'::character)';
-    private static $tomany_foreign_queries = 'select distinct ra.ordinal, rn.nspname schema_name, rt.relname table_name, ra.attname column_name, c.conname "name", n.nspname foreign_schema_name, t.relname foreign_table_name, a.attname foreign_column_name '.
+    private static $tomany_foreign_queries = 'select distinct ra.ordinal, rn.nspname schema_name, rt.relname table_name, ra.attname column_name, c.conname "name", '.
+    'n.nspname foreign_schema_name, t.relname foreign_table_name, a.attname foreign_column_name '.
     'from pg_catalog.pg_constraint c '.
     'join pg_catalog.pg_namespace n on n.oid=c.connamespace join pg_catalog.pg_class t on t.oid=c.conrelid join pg_catalog.pg_attribute a on a.attrelid=t.oid and a.attnum = any (c.conkey) '.
     'join pg_catalog.pg_class rt on rt.oid=c.confrelid join pg_catalog.pg_namespace rn on rn.oid=rt.relnamespace '.
     'join pg_catalog.pg_attribute ra on ra.attrelid=rt.oid and ra.attnum = any (c.confkey) '.
     'join pg_catalog.pg_constraint uc on rn.oid=uc.connamespace and ra.attnum = any (uc.conkey) and rt.oid=uc.conrelid and uc.contype = any (array[\'u\'::character,\'p\'::character]) '.
     'where (c.contype =\'f\'::character)';
-    private static $indices_query          = 'select n.nspname schema_name,  t.relname as table_name,  i.relname as "name",  a.attname as column_name,  ix.indisprimary is_primary,  ix.indisunique is_unique  '.
+    private static $indices_query = 'select n.nspname schema_name,  t.relname as table_name,  i.relname as "name",  a.attname as column_name,  ix.indisprimary is_primary,  ix.indisunique is_unique  '.
     'from  pg_catalog.pg_class t,  pg_catalog.pg_namespace as n,  pg_class i,  pg_index ix,  pg_attribute a  '.
     'where  t.relnamespace=n.oid  and t.oid = ix.indrelid  and i.oid = ix.indexrelid  and a.attrelid = t.oid  and a.attnum = ANY(ix.indkey)  and t.relkind = \'r\' ';
 
@@ -61,8 +62,8 @@ class PostgreSQL extends Dbms
     public function getReferencedForeignKeys($schema, $table_name = null)
     {
         $data = $table_name ? $this->tableToOneForeignKeys($schema, $table_name) : $this->schemaToOneForeignKeys($schema);
-        usort($data, function ($f, $s) { return $f['ordinal'] <=> $s['ordinal']; });
-        return array_map(function ($d) { return new ForeignKey($d, false); }, $data);
+        usort($data, function($f, $s){ return $f['ordinal'] <=> $s['ordinal']; });
+        return array_map(function($d){ return new ForeignKey($d, false); }, $data);
     }
 
     private function tableToOneForeignKeys($schema, $table_name)
@@ -96,8 +97,8 @@ class PostgreSQL extends Dbms
     public function getReferencingForeignKeys($schema, $table_name = null)
     {
         $data = $table_name ? $this->tableToManyForeignKeys($schema, $table_name) : $this->schemaToManyForeignKeys($schema);
-        usort($data, function ($f, $s) { return $f['ordinal'] <=> $s['ordinal']; });
-        return array_map(function ($d) { return new ForeignKey($d, true); }, $data);
+        usort($data, function($f, $s){ return $f['ordinal'] <=> $s['ordinal']; });
+        return array_map(function($d){ return new ForeignKey($d, true); }, $data);
     }
 
     private function tableToManyForeignKeys($schema, $table_name)
@@ -128,7 +129,7 @@ class PostgreSQL extends Dbms
     public function getConstraints($schema, $table_name = null)
     {
         $data = $table_name ? $this->tableConstraints($schema, $table_name) : $this->schemaConstraints($schema);
-        return array_map(function ($d) { return new DBConstraint($d); }, $data);
+        return array_map(function($d){ return new DBConstraint($d); }, $data);
     }
 
     private function schemaConstraints($schema)
@@ -152,7 +153,7 @@ class PostgreSQL extends Dbms
     public function getIndices($schema, $table_name = null)
     {
         $data = $table_name ? $this->tableIndices($schema, $table_name) : $this->schemaIndices($schema);
-        return array_map(function ($d) { return new DBIndex($d); }, $data);
+        return array_map(function($d){ return new DBIndex($d); }, $data);
     }
 
     private function schemaIndices($schema)
@@ -182,7 +183,7 @@ class PostgreSQL extends Dbms
         $stmt = $this->connection->prepare('select schema_name from information_schema.schemata where schema_name not like \'pg_%\' and schema_name not in (\'information_schema\') and catalog_name = :db');
         $stmt->execute([':db' => $this->currentDatabase(true)]);
         // echo $stmt->_debugQuery(true), "\n";
-        return array_map(function ($details) { return new Schema($details['schema_name'], $this->currentDatabase(true)); }, $stmt->fetchAll(\PDO::FETCH_ASSOC));
+        return array_map(function($details){ return new Schema($details['schema_name'], $this->currentDatabase(true)); }, $stmt->fetchAll(\PDO::FETCH_ASSOC));
     }
 
     /**
@@ -198,7 +199,7 @@ class PostgreSQL extends Dbms
         $stmt->execute([':ts' => $schema]);
         $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         //$fq= $stmt->_debugQuery(true);
-        return $this->mapTables(array_map(function ($details) {
+        return $this->mapTables(array_map(function($details){
             return new DBTable(array_merge(['db_name' => $this->currentDatabase(true)], $details), true);
         }, $data));
     }
@@ -211,67 +212,9 @@ class PostgreSQL extends Dbms
         $stmt->execute([':ts' => $schema]);
         $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         //echo $stmt->_debugQuery(true),"\n";
-        return $this->mapTables(array_map(function ($details) {
+        return $this->mapTables(array_map(function($details){
             return new DBTable(array_merge(['db_name' => $this->currentDatabase(true)], $details), true);
         }, $data));
-    }
-
-    /**
-     * One to one
-     *
-     * @param string $schema
-     * @param string $table_name
-     *
-     * @return ForeignKey[]
-     */
-    public function getReferencedForeignKeysxx($schema, $table_name = null)
-    {
-        $params = [':db' => $this->currentDatabase(true),];
-        $ts     = ['', ''];
-        if (is_string($table_name)) {
-            $params[':tn'] = $table_name;
-        }
-        if (is_string($schema)) {
-            $params[':ts'] = $schema;
-            $ts            = [' AND tc.table_schema=:ts', ' AND ccu.table_schema=:ts'];
-        }
-        /** @var DBRPDO_Statement $stmt */
-        $stmt = $this->connection->prepare("SELECT tc.table_schema, tc.constraint_name \"name\", tc.table_name, kcu.column_name, ccu.table_schema AS foreign_table_schema, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name, false unique_column FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema WHERE tc.constraint_type = 'FOREIGN KEY' {$ts[0]} and tc.table_catalog=:db ".(is_string($table_name) ? ' AND tc.table_name=:tn' : '').
-            'union '.
-            "select ccu.table_schema, tc.constraint_name \"name\", ccu.table_name, ccu.column_name, tc.table_schema AS foreign_table_schema, tc.table_name AS foreign_table_name, kcu.column_name AS foreign_column_name, false unique_column FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema join (select kcu.table_catalog,kcu.table_schema, kcu.column_name, kcu.table_name from information_schema.key_column_usage kcu join information_schema.table_constraints tc on tc.table_name=kcu.table_name and tc.constraint_name=kcu.constraint_name AND tc.table_schema = kcu.table_schema and tc.constraint_type='UNIQUE') t on t.table_name=tc.table_name and t.table_catalog=tc.table_catalog and t.table_schema=tc.table_schema and t.column_name=kcu.column_name WHERE tc.constraint_type = 'FOREIGN KEY' {$ts[1]} AND ccu.table_catalog=:db ".(is_string($table_name) ? ' AND ccu.table_name=:tn' : ''));
-        $stmt->execute($params);
-        // echo $stmt->_debugQuery(true), "\n";
-        return $this->mapForeignKeys(array_map(function ($details) { return new ForeignKey($details, false); }, $stmt->fetchAll(\PDO::FETCH_ASSOC)));
-    }
-
-    /**
-     * one to many
-     *
-     * @param $schema
-     * @param $table_name
-     *
-     * @return ForeignKey[]
-     */
-    public function getReferencingForeignKeysxx($schema, $table_name = null)
-    {
-        $this->switchSchema($schema);
-        $params = [':db' => $this->currentDatabase(true),];
-        if (is_string($schema)) {
-            $params[':ts'] = $schema;
-        }
-        if ($table_name && is_string($table_name)) {
-            $params[':tn'] = $table_name;
-        }
-        /** @var DBRPDO_Statement $stmt */
-        $stmt = $this->connection->prepare('SELECT false unique_column, tc.table_schema AS foreign_table_schema, tc.constraint_name "name", tc.table_name AS foreign_table_name, kcu.column_name AS foreign_column_name, '.
-            'ccu.table_schema, ccu.table_name, ccu.column_name FROM information_schema.table_constraints AS tc '.
-            'JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema '.
-            'JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema '.
-            'WHERE ccu.table_catalog=:db AND tc.constraint_type = \'FOREIGN KEY\' '.(is_string($schema) ? ' AND ccu.table_schema=:ts ' : '').
-            (is_string($table_name) ? ' AND ccu.table_name=:tn ' : ''));
-        $stmt->execute($params);
-        //echo $stmt->_debugQuery(true),"\n";
-        return $this->mapForeignKeys(array_map(function ($details) { return new ForeignKey($details, true); }, $stmt->fetchAll(\PDO::FETCH_ASSOC)));
     }
 
     /**
@@ -283,8 +226,8 @@ class PostgreSQL extends Dbms
     public function getColumns($schema, $table_name = null)
     {
         $data = $table_name ? $this->tableColumns($schema, $table_name) : $this->schemaColumns($schema);
-        usort($data, function ($f, $s) { return $f['ordinal'] <=> $s['ordinal']; });
-        return $this->mapColumns(array_map(function ($details) { return new DBColumn($details); }, $data));
+        usort($data, function($f, $s){ return $f['ordinal'] <=> $s['ordinal']; });
+        return $this->mapColumns(array_map(function($details){ return new DBColumn($details); }, $data));
     }
 
     /**
@@ -317,30 +260,6 @@ class PostgreSQL extends Dbms
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
-    protected function mapColumnsData(array $data)
-    {
-        $_data              = $data;
-        $_data['data_type'] = [];
-        foreach ($data as $name => $datum) {
-            if (0 === strcasecmp('table_schema', $name)) {
-                $_data['schema_name'] = $datum;
-            } elseif (0 === strcasecmp('column_name', $name)) {
-                $_data['name'] = $datum;
-            } elseif (0 === strcasecmp('column_default', $name)) {
-                $_data['default'] = $this->isAFunction($datum) ? null : $datum;
-            } elseif (0 === strcasecmp('character_set_name', $name)) {
-                $_data['charset'] = $datum;
-            } elseif (0 === strcasecmp('data_type', $name) || 0 === strcasecmp('udt_name', $name)) {
-                $_data['data_type'][] = $datum;
-            } elseif (0 === strcasecmp('numeric_scale', $name)) {
-                $_data['decimal_places'] = $datum;
-            } elseif (0 === strcasecmp('constraint_type', $name)) {
-                $_data['is_primary'] = 0 === strcasecmp('PRIMARY KEY', $datum);
-            }
-        }
-        return $_data;
-    }
-
     /**
      * @param bool $name
      *
@@ -350,11 +269,6 @@ class PostgreSQL extends Dbms
     {
         $this->current_db = $this->current_db ?: new Database($this->connection->query('SELECT current_database();')->fetchColumn());
         return true === $name && $this->current_db ? $this->current_db->name : $this->current_db;
-    }
-
-    public function isAFunction($name)
-    {
-        return in_array($name, ['current_timestamp']) || preg_match('/^(\w+)(\()(.*?)?(\))$/i', $name) || preg_match('/::/i', $name);
     }
 
     protected function switchSchema($schema)
